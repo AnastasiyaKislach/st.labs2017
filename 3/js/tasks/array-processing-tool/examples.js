88050e0c5f7ec2186d3afd4aee2ae40cb6f6d978
Anastasiya Kislach <anastasiya.kislach@gmail.com>
2017-07-25 18:36:35 +0300

    array-processing-tool

--- 3/js/tasks/array-processing-tool/examples.js
+++ 3/js/tasks/array-processing-tool/examples.js
@@ -11,7 +11,7 @@
         [1, 2, 3],
         [-1, -2, -3]
     ];
-
+    console.log("5. найти непрерывный подмассив arr, сумма элементов которого максимальна");
     for (var i = 0; i < arrays.length; i++) {
         console.log(arrays[i].toString());
         console.log("Макс. сумма эл-в непрерывного подмассива O(n^2): ", arrayProcessingTool.getMaxSubSum(arrays[i]));
@@ -19,6 +19,9 @@
         console.log("Максимум: ", arrayProcessingTool.max(arrays[i]));
         console.log("Минимум: ", arrayProcessingTool.min(arrays[i]));
         console.log("Медиана: ", arrayProcessingTool.median(arrays[i]));
+
+        console.log("\n");
     }
 
+    console.log("\n");
 })();
