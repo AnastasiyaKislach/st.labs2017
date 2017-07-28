766297c7b8c32d2b3d87a3c3a250af8e685cc589
Anastasiya Kislach <anastasiya.kislach@gmail.com>
2017-07-28 11:32:26 +0300

    linear-unfold

---
+++ 4/js/linear-unfold/linear-unfold.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    "use strinct";
+
+    function isEmpty(val) {
+        return (val === undefined || val == null) ? true : false;
+    }
+
+
+    function unfoldWith(callback, initialValue) {
+        var result = [];
+
+        if (isEmpty(initialValue)) {
+            return result;
+        }
+        while (initialValue){
+           result.push(initialValue); 
+            initialValue = callback(initialValue);
+        }
+        
+        return result;
+    }
+
+
+
+    function decrement(initialValue) {
+        return initialValue > 0
+                        ? initialValue - 1
+                            : false;
+    }
+
+    var initVal = 15;
+
+    var result = unfoldWith(decrement, initVal);
+
+    console.log(result);
+
+})();
