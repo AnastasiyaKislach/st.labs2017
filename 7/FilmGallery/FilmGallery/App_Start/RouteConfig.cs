912dcd80572a1d82f0cd0afa725736ae145991f6
Anastasiya Kislach <anastasiya.kislach@gmail.com>
2017-09-05 17:57:58 +0300

    add rating changing, logout link

--- 7/FilmGallery/FilmGallery/App_Start/RouteConfig.cs
+++ 7/FilmGallery/FilmGallery/App_Start/RouteConfig.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace FilmGallery
@@ -9,17 +13,17 @@ namespace FilmGallery
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-			//routes.MapRoute(
-   //             name: "Default",
-   //             url: "{controller}/{action}/{id}",
-   //             defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-   //         );
-
 			routes.MapRoute(
-				name: "Default", 
-				url: "{*url}", 
-				defaults: new { controller = "Home", action = "Index" }
-			);
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+			//routes.MapRoute(
+			//	name: "Default", 
+			//	url: "{*url}", 
+			//	defaults: new { controller = "Home", action = "Index" }
+			//);
 		}
 
     }
