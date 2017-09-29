a4ddc180bf037def54b73942cf83ee9b474ca8bc
AnastasiyaKisalch <anastasiya.kislach@gmail.com>
2017-09-29 16:04:26 +0300

    user entity

---
+++ 7/FilmGallery/FilmGallery.Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FilmGallery.Entities {
+	public class User : IdentityUser {
+		public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager) {
+			var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+			return userIdentity;
+		}
+	}
+}
